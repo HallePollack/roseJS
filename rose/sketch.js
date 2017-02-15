942c878f115d59b55996b3d25b2820429dbc2227
HallePollack <hmp47@cornell.edu>
2017-02-14 21:05:58 -0500

    Update sketch.js

--- rose/sketch.js
+++ rose/sketch.js
@@ -1,55 +1,53 @@
 
-var n;
-var nSlider;
-var dSlider;
-var d; 
+//Halle Pollack 2017
 
 function setup() {
-  createCanvas(600,600);
-  background(50);
-  
-  nSlider = createSlider(0,10,1);
-  dSlider = createSlider(0,10,1);
-
-  
+   
+//background and slider setup//
+    
+    background(51);
+    createCanvas(400, 400);
+    nSlider = createSlider(0, 10, random(10));
+    dSlider = createSlider(1, 10, random(1,10));
+    nSlider.position(width / 3, height + 20);
+    dSlider.position(width / 3, height + 40);
 }
-
-
 
 function draw() {
-  background(50);
-  
-  n = nSlider.value();
-  d = dSlider.value();
-  
-  k = n/d;
-  //k = kSlider.value();
- // print(k);
- 
-  translate(width/2, height/2);
-  beginShape()
-  
-  for (theta = 0; theta < (2 * PI)*d; theta +=.01){
- 
-    r = cos(k * theta);
+    background(51);
     
-    x = (200 * r * cos(theta));
-    y = (200 * r * sin(theta));
-  
-    stroke(255);
-    noFill();
-    vertex(x,y);
-  
- 
-  
-  }
-  endShape(CLOSE);
-  
-  
-
-  
-  
+//get values from sliders//
+    n = nSlider.value();
+    d = dSlider.value();
+    k = n / d;
+    
+ //add labels   
+    strokeWeight(1)
+    nLabel = text(n, 30, height - 30);
+    text("k = ---", 8, height - 21);
+    dLabel = text(d, 30, height - 10);
+    translate(width / 2, height / 2);
+    strokeWeight(4);
+    
+    
+    
+//WHERE THE DRAWING HAPPENS//
+    
+    beginShape()
+    
+//From 0 to 2*pi, in intervals of .01, calculate the r value and covert to x,y and plot the points (Javascript can only plot in x,y coordinates) and at the end, connect all the points to create a smooth shape// 
+    
+    
+    for (theta = 0; theta < (2 * PI) * d; theta += .01) {
+        r = cos(k * theta);
+        x = (200 * r * cos(theta));
+        y = (200 * r * sin(theta));
+        stroke(255);
+        noFill();
+        vertex(x, y);
+    }
+    endShape(CLOSE);
+    
+    
+    
 }
-  
-
-  
